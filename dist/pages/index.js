2adf5f5f5e8399ee16fdec40cf20784e15bb6285
CodeVpe <120424596@qq.com>
2018-12-10 21:12:45 +0800

    second commit

--- dist/pages/index.js
+++ dist/pages/index.js
@@ -1,23 +1,20 @@
-// require(["../scripts/config.js"],function() {
-// 	require(["common","fontscroll","jquery","swiper"],function (com,font,$,Swiper) {
-// 		
-// 		 var mySwiper = new swiper('.swiper-container',{
-// 				//无缝连接
-// 				 loop : true,
-// 				//自动切换
-// 				autoplay:true,
-// 				 navigation: {
-// 			      nextEl: '.swiper-button-next',
-// 			      prevEl: '.swiper-button-prev'
-//     		}
-// 		})
-// 		console.log($)
-// 		
-// 	})
-// })
-// 
-require(["../scripts/config.js"],function(){
-	require(["common","jquery","swiper","fontscroll"],function( com , $ , swiper){
-			console.log($);
+require(["../scripts/config.js"],function() {
+	require(["common","fontscroll","jquery","swiper"],function (com,font,$,swiper) {
+		
+		var mySwiper = new swiper('.swiper-container',{
+				//无缝连接
+				loop : true,
+				// 如果需要分页器
+				pagination: {
+				  el: '.swiper-pagination',
+				},
+				//自动切换
+				autoplay:true,
+				 navigation: {
+			      nextEl: '.swiper-button-next',
+			      prevEl: '.swiper-button-prev'
+    		}
+		})
+		
 	})
 })
